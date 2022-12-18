812f12a9d46de8e9cdd645d65ff200487e85ca0e
ShubhamBiwal <shubhambiwal21042003@gmail.com>
2022-12-18 15:21:07 +0530

    pwa update

--- serviceWorker.js
+++ serviceWorker.js
@@ -15,56 +15,18 @@ const assets = [
   //   "/images/tribute-page.jpg"
 ];
 
-self.addEventListener("install", function (event) {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(staticShubhamKumar).then(function (cache) {
-      console.log("Opened cache");
-      return cache.addAll(assets);
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticShubhamKumar).then((cache) => {
+      cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener("fetch", function (event) {
-  let online = navigator.onLine;
-  if (!online) {
-    event.respondWith(
-      caches.match(event.request).then(function (res) {
-        if (res) {
-          return res;
-        }
-      })
-    );
-  }
-});
-
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    caches.keys().then(function (keys) {
-      return prompt.all(
-        keys.map(function (keys, i) {
-          if (keys !== staticShubhamKumar) {
-            return caches.delete(keys[i]);
-          }
-        })
-      );
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
     })
   );
 });
-
-
-// self.addEventListener("install", (installEvent) => {
-//   installEvent.waitUntil(
-//     caches.open(staticShubhamKumar).then((cache) => {
-//       cache.addAll(assets);
-//     })
-//   );
-// });
-
-// self.addEventListener("fetch", (fetchEvent) => {
-//   fetchEvent.respondWith(
-//     caches.match(fetchEvent.request).then((res) => {
-//       return res || fetch(fetchEvent.request);
-//     })
-//   );
-// });
